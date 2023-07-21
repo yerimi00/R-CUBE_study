38e1f4de9430077f13c29c957105b40f0d1628cf
yerimi00 <fhddl1019@naver.com>
2023-07-21 11:16:25 +0900

    todo

--- todoList/index.js
+++ todoList/index.js
@@ -1,11 +1,15 @@
 const toDoList = document.querySelector("#to-do-list");
+const items = toDoList.children;
 
-function addNewTodo(text) {
-  const li = document.createElement("li");
-  li.textContent = text;
-  toDoList.append(li);
+// 1. updateToDo 함수를 완성해 주세요
+function updateToDo(event) {
+  event.target.classList.toggle("done");
 }
 
-addNewTodo("자바스크립트 공부하기");
-addNewTodo("고양이 화장실 청소하기");
-addNewTodo("고양이 장난감 쇼핑하기");
+// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요
+for (let item of items) {
+  item.addEventListener("click", updateToDo);
+}
+
+// 테스트 코드
+items[2].removeEventListener("click", updateToDo);
