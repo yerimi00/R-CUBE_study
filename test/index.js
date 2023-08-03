6e9ed9a82e84f0a5c75ab7074700acfb22628708
yerimi00 <fhddl1019@naver.com>
2023-08-03 09:47:37 +0900

    todo 커밋

--- test/index.js
+++ test/index.js
@@ -1,11 +1,28 @@
-// 이벤트 객체 (Event Object)
-const myInput = document.querySelector("#myInput");
-const myBtn = document.querySelector("#myBtn");
+// 이벤트 버블링 (Event Bubbling)
+const content = document.querySelector("#content");
+const title = document.querySelector("#title");
+const list = document.querySelector("#list");
+const items = document.querySelectorAll(".item");
 
-function printEvent(event) {
-  console.log(event);
-  event.target.style.color = "red";
+content.addEventListener("click", function (e) {
+  console.log("content Event");
+  console.log(e.currentTarget);
+});
+
+title.addEventListener("click", function (e) {
+  console.log("title Event");
+  console.log(e.currentTarget);
+});
+
+list.addEventListener("click", function (e) {
+  console.log("list Event");
+  console.log(e.currentTarget);
+});
+
+for (let item of items) {
+  item.addEventListener("click", function (e) {
+    console.log("item Event");
+    console.log(e.currentTarget);
+    e.stopPropagation();
+  });
 }
-
-myInput.addEventListener("keydown", printEvent);
-myBtn.addEventListener("click", printEvent);
--- todoList/index.js
+++ todoList/index.js
@@ -1,15 +1,21 @@
 const toDoList = document.querySelector("#to-do-list");
-const items = toDoList.children;
 
 // 1. updateToDo 함수를 완성해 주세요
 function updateToDo(event) {
-  event.target.classList.toggle("done");
+  if (event.target.classList.contains("item")) {
+    event.target.classList.toggle("done");
+  }
 }
 
-// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요
-for (let item of items) {
-  item.addEventListener("click", updateToDo);
-}
+// 2. 각 li 태그가 아닌 하나의 태그에만 이벤트 핸들러를 등록해 주세요
+toDoList.addEventListener("click", updateToDo);
 
 // 테스트 코드
-items[2].removeEventListener("click", updateToDo);
+const newToDo = document.createElement("li");
+newToDo.textContent = "가계부 정리하기";
+newToDo.classList.add("item");
+toDoList.append(newToDo);
+
+toDoList.children[2].addEventListener("click", function (e) {
+  e.stopPropagation();
+});
